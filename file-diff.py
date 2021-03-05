4c9ee889cf9bcf01757f5083085c3ff04f66d30c
jasonwalsh <jasonwalsh@tutanota.com>
2021-03-05 16:06:43 -0500

    fixing this stupid program

--- file-diff.py
+++ file-diff.py
@@ -1,12 +1,12 @@
 import sys
 
-# Checks to see if a line in file1 is NOT present in file2
+# Checks to see if a line in f_i_l_e_o_n_e is NOT present in file2
 # This does not take into account order of the lines within each file
-with open(sys.argv[1], 'r') as file1:
+with open(sys.argv[1], 'r') as f_i_l_e_o_n_e:
     with open(sys.argv[2],'r') as file2:
-    	# Protip: If you want to reverse this, and see what lines in file1 are present in file2
+                                                                                                                                                                                        # Protip: If you want to reverse this, and see what lines in f_i_l_e_o_n_e are present in file2
     	# change .difference() to .intersection()
-        same = set(file1).difference(file2)
+        same = set(f_i_l_e_o_n_e).difference(file2)
         
 #optional
 #same.discard('\n')
