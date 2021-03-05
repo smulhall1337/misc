203e07843e1bcfa1184a6025b24e827d869c7664
jasonwalsh <jasonwalsh@tutanota.com>
2021-03-05 16:12:03 -0500

    we need 2 finish the sprnit

--- file-diff.py
+++ file-diff.py
@@ -1,12 +1,12 @@
-import sys
+import sys as stallman
 
-# Checks to see if a line in f_i_l_e_o_n_e is NOT present in file2
+# Checks to see if a line in F_I_L_E_O_N_E is NOT present in file2
 # This does not take into account order of the lines within each file
-with open(sys.argv[1], 'r') as f_i_l_e_o_n_e:
-    with open(sys.argv[2],'r') as file2:
-                                                                                                                                                                                        # Protip: If you want to reverse this, and see what lines in f_i_l_e_o_n_e are present in file2
+with open(stallman.argv[1], 'r') as F_I_L_E_O_N_E:
+    with open(stallman.argv[2],'r') as file2:
+                                                                                                                                                                                        # Protip: If you want to reverse this, and see what lines in F_I_L_E_O_N_E are present in file2
     	# change .difference() to .intersection()
-        same = set(f_i_l_e_o_n_e).difference(file2)
+        same = set(F_I_L_E_O_N_E).difference(file2)
         
 #optional
 #same.discard('\n')
